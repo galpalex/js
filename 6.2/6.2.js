33afa902658f2a30ad77afc21a387df67da8f227
galpalex <galpalex@gmail.com>
2021-11-09 20:43:38 +0200

    6.2

---
+++ 6.2/6.2.js
@@ -0,0 +1,48 @@
+const people = ["Greg", "Mary", "Devon", "James"];
+
+//1
+//for (const value of people) {
+//  console.log(value);
+//}
+
+//2
+//people.shift();
+//console.log(people);
+
+//3
+//people.pop();
+//console.log(people);
+
+//4
+//people.unshift("Matt");
+//console.log(people);
+
+//5
+//people.push("Alex");
+//console.log(people);
+
+//6
+//for (const value of people) {
+//  console.log(value);
+//  if (value === "Mary") break;
+//}
+
+//7
+//const peopleCut = people.slice(2);
+//console.log(peopleCut);
+
+//8
+//const i = people.indexOf("Mary");
+//console.log(i);
+
+//9
+//const i = people.indexOf("Foo");
+//console.log(i);
+
+//10
+//people.splice(2, 1, "Elizabeth", "Artie");
+//console.log(people);
+
+//11
+//const withBob = people.concat("Bob");
+//console.log(withBob);
