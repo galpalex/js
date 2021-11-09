c16bb60cfe13c81dadbcdd857f1fdec51a2912f4
galpalex <galpalex@gmail.com>
2021-11-09 20:42:08 +0200

    5.4

---
+++ 5.4/5.4.js
@@ -0,0 +1,17 @@
+const pickColor = (color) => {
+  switch (color.toLowerCase()) {
+    case "yellow":
+    case "pink":
+    case "orange":
+      return "light color";
+    case "blue":
+    case "purple":
+    case "brown":
+      return "dark color";
+    default:
+      return "Unknown color";
+  }
+};
+console.log(pickColor("pink"));
+console.log(pickColor("brown"));
+console.log(pickColor("rose"));
